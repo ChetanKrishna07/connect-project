a156128309468505220ec3e3ff33a44fc750c546
Chetan Krishna <chetanpalicherla@gmail.com>
2022-10-19 06:12:55 +0530

    integrated photos with backend

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,6 @@ require('dotenv/config')
 
 const app = express()
 app.use(cors())
-// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}))
-
-// app.use(bodyParser.json({limit: '100mb'}));
-// app.use(express.urlencoded({limit: '100mb'}))
 
 
 app.use(express.json({limit: '50mb'}))
